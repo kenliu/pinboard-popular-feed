85eb2d12b667dead3eb9affb53c02973965409d8
Kenneth Liu <ken.liu@gmail.com>
2023-04-01 14:58:58 -0400

    code cleanup and restructuring, improve logging and error handling

--- main.go
+++ main.go
@@ -1,29 +1,73 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"pinboard-popular-feed/data"
 )
 
-func buildMastodonCredentials() MastodonCredentials {
+func main() {
+	mastodonCredentials, err := buildMastodonCredentials()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	popular, err := fetchCurrentPinboardPopular()
+	if err != nil {
+		println("error scraping popular bookmarks")
+		os.Exit(1)
+	}
+
+	// initialize the bookmark store
+	db := data.Init()
+	db.InitStore(data.DBConfig{})
+
+	postCount, err := postNewLinks(popular, db, mastodonCredentials)
+	if err != nil {
+		println("error posting new links")
+		os.Exit(1)
+	}
+	println("posted " + fmt.Sprint(postCount) + " new bookmarks")
+}
+
+func buildMastodonCredentials() (MastodonCredentials, error) {
 	if os.Getenv("MASTODON_ACCESS_TOKEN") == "" {
 		println("MASTODON_ACCESS_TOKEN not set")
-		os.Exit(1)
+		return MastodonCredentials{}, errors.New("MASTODON_ACCESS_TOKEN not set")
 	}
 
 	if os.Getenv("MASTODON_SERVER_DOMAIN") == "" {
 		println("MASTODON_SERVER_DOMAIN not set")
-		os.Exit(1)
+		return MastodonCredentials{}, errors.New("MASTODON_SERVER_DOMAIN not set")
 	}
 
 	return MastodonCredentials{
 		accessToken:  os.Getenv("MASTODON_ACCESS_TOKEN"),
 		serverDomain: os.Getenv("MASTODON_SERVER_DOMAIN"),
-	}
+	}, nil
 }
 
-func main() {
+func postNewLinks(popular []*data.Bookmark, db data.BookmarkStore, mastodonCredentials MastodonCredentials) (int, error) {
+	var postCount int
+	for i := 0; i < len(popular); i++ {
+		found, err := db.FindBookmark(popular[i].Id)
+		if err != nil {
+			panic(err) //TODO handle this error
+		}
+		if !found {
+			db.StoreBookmark(*popular[i])
+			println("new bookmark stored")
+			postCount++
+			TootBookmark(*popular[i], mastodonCredentials)
+		}
+		// TODO implement some kind of rate limiting?
+		// https://docs.joinmastodon.org/api/rate-limits/
+	}
+	return postCount, nil
+}
+
+func fetchCurrentPinboardPopular() ([]*data.Bookmark, error) {
 	popular := ScrapePinboardPopular()
 	println("current popular bookmarks: ")
 	for i := 0; i < len(popular); i++ {
@@ -34,25 +78,5 @@ func main() {
 	}
 
 	println("found " + fmt.Sprint(len(popular)) + " bookmarks on pinboard popular")
-
-	// initialize the bookmark store
-	db := data.Init()
-	db.InitStore(data.DBConfig{})
-
-	var postCount int
-	for i := 0; i < len(popular); i++ {
-		found, err := db.FindBookmark(popular[i].Id)
-		if err != nil {
-			panic(err)
-		}
-		if !found {
-			db.StoreBookmark(*popular[i])
-			println("new bookmark stored")
-			postCount++
-			TootBookmark(*popular[i], buildMastodonCredentials())
-		}
-		// TODO implement some kind of rate limiting?
-		// https://docs.joinmastodon.org/api/rate-limits/
-	}
-	println("posted " + fmt.Sprint(postCount) + " new bookmarks")
+	return popular, nil //TODO handle any errors
 }
